3e8159129d9b89615647dff32bf35bc94fbe2366
carjug@gmail.com <carly@metabrite.com>
2018-02-09 12:06:32 -0800

    Add test to make sure all columns are present in dataframe

--- tests/test_bank_statement.py
+++ tests/test_bank_statement.py
@@ -7,4 +7,11 @@ class TestBankStatement(unittest.TestCase):
 
     def test_load_csv(self):
         results = BankStatement.load_data('./test_data.csv')
-        self.assertTrue(results)
+        self.assertIsNotNone(results)
+
+    def test_load_csv_dataframe_values(self):
+        expected_columns = ['date', 'from_account', 'to_account', 'amount']
+        dataframe = BankStatement.load_data('./test_data.csv')
+        actual_columns = list(dataframe)
+
+        self.assertEqual(expected_columns, actual_columns)
