3a46e9c56fe20579ddb5282b5297a531ba1d66b5
carjug@gmail.com <carly@metabrite.com>
2018-02-09 11:50:14 -0800

    Extract calculation into separate class method

--- ledger/bank_statement.py
+++ ledger/bank_statement.py
@@ -6,7 +6,23 @@ class BankStatement(object):
 
     def balance_inquiry(self, date, account_name, file_path):
         data = self.load_data(file_path)
-        return data
+        account_value = self.calculate_balance(account_name, data, date)
+
+        return "The balance for {}'s account is {}".format(account_name, format(account_value, '.2f'))
+
+    @classmethod
+    def calculate_balance(cls, account_name, data, date):
+        account_value = 0.00
+        for index, row in data.iterrows():
+            if row['date'] <= date:
+                if row['from_account'] == account_name:
+                    # debit the account
+                    account_value -= row['amount']
+
+                if row['to_account'] == account_name:
+                    # credit the account
+                    account_value += row['amount']
+        return account_value
 
     @classmethod
     def load_data(cls, file_path):
