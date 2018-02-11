17c4bf35a2390a4ce613d6f5cdb1698c966cb5ef
carjug@gmail.com <carly@metabrite.com>
2018-02-11 14:37:54 -0800

    Initialize BankStatement with account data

--- ledger/bank_statement.py
+++ ledger/bank_statement.py
@@ -6,7 +6,17 @@ class BankStatement(object):
     TODO: Fill this out
     """
 
-    def balance_inquiry(self, date, account_name, file_path):
+    def __init__(self, file_path):
+        """
+        Initialize BankStatement class with .csv data
+
+        :param file_path: a path to a .csv file containing all cross-account transactional data
+        :type file_path: str
+        """
+
+        self.data = CSVLoader.load_as_dataframe(file_path)
+
+    def balance_inquiry(self, date, account_name):
         """
         Loads csv data, calculates the balance, and returns a formatted string containing the account information
 
@@ -14,19 +24,15 @@ class BankStatement(object):
         :type date: str
         :param account_name: the account name for whom to calculate the balance
         :type account_name: str
-        :param file_path: a path to a .csv file containing all cross-account transactional data
-        :type file_path: str
 
         :returns: a formatted string
         """
 
-        data = CSVLoader.load_as_dataframe(file_path)
-        account_value = self.calculate_balance(date, account_name, data)
+        account_value = self.calculate_balance(date, account_name)
 
         return "The balance for {}'s account on {} is {}".format(account_name, date, format(account_value, '.2f'))
 
-    @classmethod
-    def calculate_balance(cls, date, account_name, data):
+    def calculate_balance(self, date, account_name):
         """
         Calculates and returns the balance for the account on the date supplied
 
@@ -34,8 +40,6 @@ class BankStatement(object):
         :type date: str
         :param account_name: the account name for whom to calculate the balance
         :type account_name: str
-        :param data: all cross-account transactional data
-        :type data: pandas.DataFrame
 
         :returns: a float of the balance
         """
@@ -49,7 +53,7 @@ class BankStatement(object):
         # initialize all accounts at 0.00 per the exercise specifications
         account_value = 0.00
 
-        for index, row in data.iterrows():
+        for index, row in self.data.iterrows():
             # only calculate rows for which the date is earlier or equal to the date specified by the user
             if row['date'] <= date:
                 if row['from_account'] == account_name:
--- tests/test_bank_statement.py
+++ tests/test_bank_statement.py
@@ -1,46 +1,42 @@
 import unittest
 
 from ledger.bank_statement import BankStatement
-from file_loaders.csv_loader import CSVLoader
 
 
 class TestBankStatement(unittest.TestCase):
     TestDataPath = './test_data/test_data.csv'
 
+    def setUp(self):
+        self.bank_statement = BankStatement(self.TestDataPath)
+
     def test_calculate_balance(self):
         """Test BankStatement.balance_calculation() for account name: 'john'"""
-
-        df = CSVLoader.load_as_dataframe(self.TestDataPath)
-        balance = BankStatement.calculate_balance('2015-02-01', 'john', df)
+        balance = self.bank_statement.calculate_balance('2015-02-01', 'john')
         self.assertEqual(-115.00, balance)
 
     def test_calculate_balance_for_unknown_account_name(self):
         """Test BankStatement.balance_calculation() for an account name that is not included in the data set"""
 
-        df = CSVLoader.load_as_dataframe(self.TestDataPath)
-        balance = BankStatement.calculate_balance('2015-02-01', 'marie', df)
+        balance = self.bank_statement.calculate_balance('2015-02-01', 'marie')
         self.assertEqual(0.00, balance)
 
     def test_calculate_balance_for_early_date(self):
         """Test BankStatement.balance_calculation() for a date earlier than the earliest date in our data set"""
 
-        df = CSVLoader.load_as_dataframe(self.TestDataPath)
-        balance = BankStatement.calculate_balance('1989-01-01', 'john', df)
+        balance = self.bank_statement.calculate_balance('1989-01-01', 'john')
         self.assertEqual(0.00, balance)
 
     def test_calculate_balance_for_null_account_name(self):
         """Test BankStatement.balance_calculation() when account name is None"""
 
-        df = CSVLoader.load_as_dataframe(self.TestDataPath)
-        self.assertRaises(ValueError, BankStatement.calculate_balance, '2017-01-01', None, df)
+        self.assertRaises(ValueError, self.bank_statement.calculate_balance, '2017-01-01', None)
 
     def test_calculate_balance_for_null_date(self):
         """Test BankStatement.balance_calculation() when date is None"""
 
-        df = CSVLoader.load_as_dataframe(self.TestDataPath)
-        self.assertRaises(ValueError, BankStatement.calculate_balance, None, 'john', df)
+        self.assertRaises(ValueError, self.bank_statement.calculate_balance, None, 'john')
 
     def test_balance_inquiry(self):
         """Test BankStatement().balance_inquiry() for account name: 'mary', date: '2017-01-15'"""
-        results = BankStatement().balance_inquiry('2017-01-15', 'mary', self.TestDataPath)
+        results = self.bank_statement.balance_inquiry('2017-01-15', 'mary')
         self.assertEqual(results, "The balance for mary's account on 2017-01-15 is 25.00")
