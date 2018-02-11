26b11567f53a4f449f91a75ba0c569d744f314c8
carjug@gmail.com <carly@metabrite.com>
2018-02-10 22:04:04 -0800

    Add balance calculation tests

--- tests/test_bank_statement.py
+++ tests/test_bank_statement.py
@@ -1,17 +1,36 @@
 import unittest
 
 from ledger.bank_statement import BankStatement
+from utils.csv_utils import CSVLoader
 
 
 class TestBankStatement(unittest.TestCase):
+    TestDataDir = './test_data/test_data.csv'
 
-    def test_load_csv(self):
-        results = BankStatement.load_csv_as_dataframe('./test_data.csv')
-        self.assertIsNotNone(results)
+    def test_calculate_balance(self):
+        """"""
+        df = CSVLoader.load_as_dataframe(self.TestDataDir)
+        balance = BankStatement.calculate_balance('john', df, '2015-02-01')
+        self.assertEqual(-115.00, balance)
 
-    def test_load_csv_dataframe_values(self):
-        expected_columns = ['date', 'from_account', 'to_account', 'amount']
-        dataframe = BankStatement.load_csv_as_dataframe('./test_data.csv')
-        actual_columns = list(dataframe)
+    def test_calculate_balance_for_unknown_account_name(self):
+        """"""
+        df = CSVLoader.load_as_dataframe(self.TestDataDir)
+        balance = BankStatement.calculate_balance('marie', df, '2015-02-01')
+        self.assertEqual(0.00, balance)
 
-        self.assertEqual(expected_columns, actual_columns)
+    def test_calculate_balance_for_early_date(self):
+        """"""
+        df = CSVLoader.load_as_dataframe(self.TestDataDir)
+        balance = BankStatement.calculate_balance('john', df, '1989-01-01')
+        self.assertEqual(0.00, balance)
+
+    def test_calculate_balance_for_null_account_name(self):
+        """"""
+        df = CSVLoader.load_as_dataframe(self.TestDataDir)
+        self.assertRaises(ValueError, BankStatement.calculate_balance, None, df, '2017-01-01')
+
+    def test_calculate_balance_for_null_date(self):
+        """"""
+        df = CSVLoader.load_as_dataframe(self.TestDataDir)
+        self.assertRaises(ValueError, BankStatement.calculate_balance, 'john', df, None)
