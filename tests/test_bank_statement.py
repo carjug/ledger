d9a63a7c7b4b21e02316ff6a3030817ec1aea56a
carjug@gmail.com <carly@metabrite.com>
2018-02-11 14:18:39 -0800

    Modify test docstring to be more clear about what we're testing

--- tests/test_bank_statement.py
+++ tests/test_bank_statement.py
@@ -8,34 +8,34 @@ class TestBankStatement(unittest.TestCase):
     TestDataPath = './test_data/test_data.csv'
 
     def test_calculate_balance(self):
-        """Test balance calculation for account name: 'john'"""
+        """Test BankStatement.balance_calculation() for account name: 'john'"""
 
         df = CSVLoader.load_as_dataframe(self.TestDataPath)
         balance = BankStatement.calculate_balance('2015-02-01', 'john', df)
         self.assertEqual(-115.00, balance)
 
     def test_calculate_balance_for_unknown_account_name(self):
-        """Test balance calculation for an account name that is not included in the data set"""
+        """Test BankStatement.balance_calculation() for an account name that is not included in the data set"""
 
         df = CSVLoader.load_as_dataframe(self.TestDataPath)
         balance = BankStatement.calculate_balance('2015-02-01', 'marie', df)
         self.assertEqual(0.00, balance)
 
     def test_calculate_balance_for_early_date(self):
-        """Test balance calculation for a date earlier than the earliest date in our data set"""
+        """Test BankStatement.balance_calculation() for a date earlier than the earliest date in our data set"""
 
         df = CSVLoader.load_as_dataframe(self.TestDataPath)
         balance = BankStatement.calculate_balance('1989-01-01', 'john', df)
         self.assertEqual(0.00, balance)
 
     def test_calculate_balance_for_null_account_name(self):
-        """Test balance calculation when account name is None"""
+        """Test BankStatement.balance_calculation() when account name is None"""
 
         df = CSVLoader.load_as_dataframe(self.TestDataPath)
         self.assertRaises(ValueError, BankStatement.calculate_balance, '2017-01-01', None, df)
 
     def test_calculate_balance_for_null_date(self):
-        """Test balance calculation when date is None"""
+        """Test BankStatement.balance_calculation() when date is None"""
 
         df = CSVLoader.load_as_dataframe(self.TestDataPath)
         self.assertRaises(ValueError, BankStatement.calculate_balance, None, 'john', df)
