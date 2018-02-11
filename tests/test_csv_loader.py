69309d471f9e68a16fe8c8b91100ee7d02ac9718
carjug@gmail.com <carly@metabrite.com>
2018-02-11 15:40:14 -0800

    Add docs for csv loader tests

--- tests/test_csv_loader.py
+++ tests/test_csv_loader.py
@@ -16,7 +16,8 @@ class TestCSVLoader(unittest.TestCase):
         self.assertIsInstance(results, pandas.DataFrame)
 
     def test_load_as_dataframe_values(self):
-        """"""
+        """Test that CSVLoader.load_as_dataframe returns a dataframe with the correct column names"""
+
         expected_columns = ['date', 'from_account', 'to_account', 'amount']
         dataframe = CSVLoader.load_as_dataframe(tbs.TestDataPath)
         actual_columns = list(dataframe)
@@ -24,7 +25,8 @@ class TestCSVLoader(unittest.TestCase):
         self.assertEqual(expected_columns, actual_columns)
 
     def test_load_as_dataframe_bad_file_path(self):
-        """"""
+        """Test CSVLoader.load_as_dataframe raises with bad input"""
+
         # assert that an IOError is raised if the .csv file cannot be found
         self.assertRaises(IOError, CSVLoader.load_as_dataframe, 'non_existent_test_file.csv')
 
