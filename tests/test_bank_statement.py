8a68ef69bf5f8240151dfae9154490b78e31e8e4
carjug@gmail.com <carly@metabrite.com>
2018-02-12 12:14:04 -0800

    Bulk up tests surrounding date input

--- tests/test_bank_statement.py
+++ tests/test_bank_statement.py
@@ -33,17 +33,41 @@ class TestBankStatement(unittest.TestCase):
         balance = self.bank_statement.calculate_balance('1989-01-01', 'john')
         self.assertEqual(0.00, balance)
 
-    def test_calculate_balance_for_null_account_name(self):
+    def test_balance_inquiry_for_full_date_range(self):
+        """Test BankStatement().balance_inquiry() for account name: 'mary', date: '2017-01-15'"""
+
+        results = self.bank_statement.balance_inquiry('2017-01-15', 'mary')
+        self.assertEqual(results, "The balance for mary's account on 2017-01-15 is 45.99")
+
+    def test_balance_inquiry_with_date_in_date_range(self):
+        """Test BankStatement().balance_inquiry() for account name: 'alice', date: '2015-01-10'"""
+
+        results = self.bank_statement.balance_inquiry('2015-01-15', 'alice')
+        self.assertEqual(results, "The balance for alice's account on 2015-01-15 is 1.00")
+
+    def test_balance_inquiry_with_date_on_transaction_date(self):
+        """Test BankStatement().balance_inquiry() when input date and transaction date are equal"""
+
+        results = self.bank_statement.balance_inquiry('2015-01-16', 'mary')
+        self.assertEqual(results, "The balance for mary's account on 2015-01-16 is 145.99")
+
+    def test_balance_inquiry_for_null_account_name(self):
         """Test BankStatement.balance_inquiry() when account name is None"""
 
         self.assertRaises(ValueError, self.bank_statement.balance_inquiry, '2017-01-01', None)
 
-    def test_calculate_balance_for_null_date(self):
+    def test_balance_inquiry_for_account_name_not_in_data_set(self):
+        """Test BankStatement.balance_inquiry() when account name is not in the data set"""
+
+        self.assertRaises(ValueError, self.bank_statement.balance_inquiry, '2017-01-01', 'mari')
+
+    def test_balance_inquiry_for_null_date(self):
         """Test BankStatement.balance_inquiry() when date is None"""
 
         self.assertRaises(ValueError, self.bank_statement.balance_inquiry, None, 'john')
 
-    def test_balance_inquiry(self):
-        """Test BankStatement().balance_inquiry() for account name: 'mary', date: '2017-01-15'"""
-        results = self.bank_statement.balance_inquiry('2017-01-15', 'mary')
-        self.assertEqual(results, "The balance for mary's account on 2017-01-15 is 25.00")
+    def test_balance_inquiry_for_improper_date_format(self):
+        """Test BankStatement.balance_inquiry() when date is improperly formatted"""
+
+        self.assertRaises(ValueError, self.bank_statement.balance_inquiry, '01-03-2015', 'john')
+
