2cfcfdac7e67a086c498d0ce9096b7180495d541
carjug@gmail.com <carly@metabrite.com>
2018-02-14 11:08:13 -0800

    Fix typo in test docstrings

--- tests/test_bank_statement.py
+++ tests/test_bank_statement.py
@@ -38,19 +38,19 @@ class TestBankStatement(unittest.TestCase):
         self.assertEqual(0.00, balance)
 
     def test_balance_inquiry_for_full_date_range(self):
-        """Test BankStatement().balance_inquiry() for account name: 'mary', date: '2017-01-15'"""
+        """Test BankStatement.balance_inquiry() for account name: 'mary', date: '2017-01-15'"""
 
         results = self.bank_statement.balance_inquiry('2017-01-15', 'mary')
         self.assertEqual(results, "The balance for mary's account on 2017-01-15 is 45.99")
 
     def test_balance_inquiry_with_date_in_date_range(self):
-        """Test BankStatement().balance_inquiry() for account name: 'alice', date: '2015-01-10'"""
+        """Test BankStatement.balance_inquiry() for account name: 'alice', date: '2015-01-10'"""
 
         results = self.bank_statement.balance_inquiry('2015-01-15', 'alice')
         self.assertEqual(results, "The balance for alice's account on 2015-01-15 is -1.65")
 
     def test_balance_inquiry_with_date_on_transaction_date(self):
-        """Test BankStatement().balance_inquiry() when input date and transaction date are equal"""
+        """Test BankStatement.balance_inquiry() when input date and transaction date are equal"""
 
         results = self.bank_statement.balance_inquiry('2015-01-16', 'mary')
         self.assertEqual(results, "The balance for mary's account on 2015-01-16 is 145.99")
